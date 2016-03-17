61ff1c7d3c6fe933d4d2939f2115a96323208587
Alexander <tim96@yandex.ru>
2016-03-17 22:12:23 +0400

    Add script file to install 'gulp' and 'bower'.

---
+++ frontend.sh
@@ -0,0 +1,17 @@
+
+npm install -g bower
+
+bower install jquery
+bower install bootstrap
+
+npm install --global gulp
+npm install --save-dev gulp
+
+npm install gulp-if
+npm install gulp-uglify
+npm install gulp-uglifycss
+npm install gulp-less
+npm install gulp-concat
+npm install gulp-sourcemaps
+
+gulp
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var env = process.env.type;
+
+gulp.task('js', function () {
+    return gulp.src([
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js',
+            'src/Tim/FoodRestaurantBundle/Resources/public/js/**/*.js'])
+        .pipe(concat('app.js'))
+        // .pipe(gulpif(env === 'prod', uglify()))
+        .pipe(uglify())
+        // .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('web/js'));
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+            'bower_components/bootstrap/dist/css/bootstrap.css',
+            'src/Tim/FoodRestaurantBundle/Resources/public/css/**/*.css'])
+        .pipe(gulpif(/[.]less/, less()))
+        .pipe(concat('styles.css'))
+        // .pipe(gulpif(env === 'prod', uglifycss()))
+        // .pipe(sourcemaps.write('./'))
+        .pipe(uglifycss())
+        .pipe(gulp.dest('web/css'));
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/Tim/FoodRestaurantBundle/Resources/public/img/**/*.*')
+        .pipe(gulp.dest('web/img'));
+});
+
+//define executable tasks when running "gulp" command
+gulp.task('default', ['js', 'css', 'img']);
